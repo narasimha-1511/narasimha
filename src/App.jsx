1453f93ba9a38c3055ec925f35bc24b7c3574da1
narasimha-1511 <s.narasimha.2005@gmail.com>
2024-05-31 16:02:00 +0530

    setup done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import NavBar from "./components/NavBar";
+import "./App.css";
+import Typing from "./components/Typing";
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
