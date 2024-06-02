7b1bf9845c8427ece271b2ca1352e4a63dfaa32d
narasimha-1511 <s.narasimha.2005@gmail.com>
2024-06-02 10:12:09 +0530

    added night sky effect

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,26 @@ import TopMove from "./components/TopMoveAlways";
 import Home from "./components/Home";
 import FooterCustom from "./components/Footer";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
+import nightsky from "./Lottie/nightsky.json";
+import Lottie from "lottie-react";
 
 function App() {
   return (
-    <div>
-      <BrowserRouter>
-        <NavBar />
-        <TopMove />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<h1>About</h1>} />
-          <Route path="/projects" element={<h1>Projects</h1>} />
-          <Route path="/resume" element={<h1>Contact</h1>} />
-        </Routes>
-        <FooterCustom />
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <Lottie className="bg" animationData={nightsky} loop={true} />
+      <Lottie className="bgtwo" animationData={nightsky} loop={true} />
+      <Lottie className="bgtemp" animationData={nightsky} loop={true} />
+
+      <NavBar />
+      <TopMove />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<h1>About</h1>} />
+        <Route path="/projects" element={<h1>Projects</h1>} />
+        <Route path="/resume" element={<h1>Contact</h1>} />
+      </Routes>
+      <FooterCustom />
+    </BrowserRouter>
   );
 }
 
